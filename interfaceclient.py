dd02e530a60bfaa20b7544979f47f9f2afe52240
si21lvi <silvia.coy.2019@upb.edu.co>
2023-02-17 10:44:53 -0500

    Create interfaceClient.py

---
+++ interfaceclient.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 21:29:28 2023
+
+@author: Silvia
+"""
+
+from abc import abstractmethod
+from abc import ABCMeta
+
+class interfaceClient(metaclass=ABCMeta):
+    
+    @abstractmethod
+    def signIn(self,user:str,password:str) ->str:
+        pass
+    @abstractmethod
+    def authentication(self,token:str) ->bool:
+        pass
+    @abstractmethod
+    def signUp(self,user:str,password:str,name:str,email:str) ->bool:
+        pass
+    @abstractmethod
+    def forgotPass(self,email:str) ->str:
+        pass
+    @abstractmethod
+    def changePass(self,actualPass:str,newPass:str,confirmNewPass:str) ->bool:
+        pass
+    @abstractmethod
+    def signOff(self,token:str) ->bool:
+        pass
+    @abstractmethod
+    def editUser(self,name:str) ->str:
+        pass
+    @abstractmethod
+    def loadFiles(self,token:str,files:list,) ->str:
+        pass
+    ""
