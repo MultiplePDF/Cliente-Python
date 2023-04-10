6c1cd4ba5c7777317eee6c6814d21dc8497b7c23
JohanCala <johan.cala.2019@upb.edu.co>
2023-04-10 16:54:53 -0500

    Cambios
    Se mejoro la vista se subir archivos, y se  arreglo el json

--- MultiplePDF/moduloArchivos/urls.py
+++ MultiplePDF/moduloArchivos/urls.py
@@ -4,5 +4,8 @@ from moduloArchivos import views
 
 urlpatterns = [
     path('Home/', views.subir_archivos, name='homeArchivos'),
-    path('DragDrop/', views.drag_and_drop, name='dragDrop')
+    path('DragDrop/', views.drag_and_drop, name='dragDrop'),
+    path('Upload/', views.upload_view, name='upload'),
+    path('Json/', views.json_view, name='json'),
+    path('Json/template/', views.json_template_view, name='json_template')
 ]
--- MultiplePDF/moduloArchivos/views.py
+++ MultiplePDF/moduloArchivos/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -13,7 +13,7 @@ from .models import Archivo
 def drag_and_drop(request):
     if not request.session.get('token'):
         # Si el token no está en la sesión, redirigir al usuario al formulario de login
-        return redirect('login')
+        return redirect('login2')
     else:
         # Si el usuario está autenticado, mostrar la página dragDrop
         return render(request, 'drag_and_drop.html')
@@ -37,3 +37,36 @@ def subir_archivos(request):
         mensaje = ''
     archivos = Archivo.objects.all()
     return render(request, 'subirArchivos.html', {'form': form, 'archivos': archivos, 'mensaje': mensaje})
+
+def upload_view(request):
+    if request.method == 'POST' and request.FILES.getlist('file'):
+        files = request.FILES.getlist('file')
+        file_data = []
+        for i, file in enumerate(files):
+            file_data.append({
+                'ID': i + 1,
+                'NombreDelDocumento': file.name,
+                'Contenido': base64.b64encode(file.read()).decode(),
+                'Size': file.size, #KB
+            })
+        request.session['file_data'] = file_data
+        return JsonResponse({'files': file_data})
+    return render(request, 'drag_and_drop.html')
+
+def json_view(request):
+    if request.method == 'POST':
+        request.session.pop('file_data', None)
+        return JsonResponse({'message': 'Los archivos cargados se eliminaron correctamente.'})
+    file_data = request.session.get('file_data', None)
+    if file_data is not None:
+        return JsonResponse({'files': file_data})
+    return JsonResponse({'error': 'No se encontró ningún archivo cargado.'})
+
+def json_template_view(request):
+    file_data = request.session.get('file_data', None)
+    if file_data is not None:
+        json_data = {'files': file_data}
+    else:
+        json_data = {'error': 'No se encontró ningún archivo cargado.'}
+    return render(request, 'MostrarJson.html', {'json_data': json_data})
+
